cafa5e0e9c739b68cd153dc3920355d1a46b69a0
xymz <57760786+xymz-dev@users.noreply.github.com>
2026-07-24 02:51:26 +0700

    Delete folder item frontend/components/ui/skeleton.tsx

--- frontend/components/ui/skeleton.tsx
+++
@@ -1,6 +0,0 @@
-import { cn } from "@/lib/utils"
-
-function Skeleton({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) {
-  return <div className={cn("animate-pulse rounded-xl bg-white/[0.05]", className)} {...props} />
-}
-export { Skeleton }
